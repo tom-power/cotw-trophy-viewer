c77eca1df1e72bd7ec4811c5e07b4632fb21451a
Tom Power <tpower21@gmail.com>
2025-09-14 11:52:52 +0100

    script fixes

--- sh/build.bat
+++ sh/build.bat
@@ -1,4 +1,6 @@
 @echo off
 call .venv\Scripts\activate.bat
 cd cotw-trophy-viewer
+rmdir /s /q build
+rmdir /s /q dist
 nicegui-pack --onefile --windowed --name "cotwTrophyViewer" mainNative.py
--- sh/build.sh
+++ sh/build.sh
@@ -1,3 +1,5 @@
 source .venv/bin/activate &&
 cd ./cotw-trophy-viewer &&
+rm -rf ./build &&
+rm -rf ./dist &&
 nicegui-pack --onefile --windowed --name "cotwTrophyViewer" mainNative.py
