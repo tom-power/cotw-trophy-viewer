4af2727b4c2fab7295e6abc34b8f216e4d0b1c4e
Tom Power <tpower21@gmail.com>
2025-09-15 08:56:16 +0100

    fix

--- sh/runDockerDeploy.sh
+++ sh/runDockerDeploy.sh
@@ -1,3 +1,3 @@
 cd ./docker &&
 docker compose -f docker-compose-deploy.yml build cotw_trophy_viewer &&
-docker compose up  -f docker-compose-deploy.yml --detach --force-recreate --remove-orphans cotw_trophy_viewer
+docker compose -f docker-compose-deploy.yml up --detach --force-recreate --remove-orphans cotw_trophy_viewer
