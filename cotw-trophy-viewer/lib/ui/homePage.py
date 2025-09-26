ec07d33f6fcfbec486c2bd90e703df4cb4d4f4a4
Tom Power <tpower21@gmail.com>
2025-09-26 07:26:32 +0100

    tidy

--- cotw-trophy-viewer/lib/ui/homePage.py
+++ cotw-trophy-viewer/lib/ui/homePage.py
@@ -11,19 +11,112 @@ from lib.ui.utils.queries import Queries
 from lib.ui.utils.rowData import rowData
 
 
-def homePage(paths=Paths(get_save_path())):
-    trophyFileExists = paths.getLoadPath() and paths.getLoadPath().exists()
-    queries = Queries()
-    db = Db(paths.getLoadPath())
+class HomePage:
+    @binding.bindable_dataclass
+    class PresetName:
+        text: str
 
-    def getDb():
-        nonlocal db
-        return db
+    def __init__(self, paths: Paths):
+        self.paths = paths
+        self.queries = Queries()
+        self.db = Db(self.paths.getLoadPath())
+        self.trophyFileExists = self.paths.getLoadPath() and self.paths.getLoadPath().exists()
 
-    def getRowData() -> list[dict]:
-        return rowData(getDb().trophyAnimals(queries.queryDict))
+        self.presetName = self.PresetName(text="")
 
-    async def uploadLodge(e):
+        self.selectLodges = None
+        self.radioReservesAndOr = None
+        self.selectReserves = None
+        self.radioMedalsAndOr = None
+        self.selectMedals = None
+        self.radioAnimalsAndOr = None
+        self.selectAnimals = None
+        self.checkboxAllAnimals = None
+
+        self.upload_component = None
+        self.selectPresets = None
+        self.addPresetDialog = None
+        self.dataGrid = None
+
+        self._build_ui()
+
+    def _build_ui(self):
+        self.addPresetDialog = ui.dialog()
+        with self.addPresetDialog, ui.card():
+            ui.input(label="preset name").bind_value(self.presetName, "text")
+            ui.button(text="save", on_click=self._addPreset)
+
+        with ui.grid(columns='3fr 1fr').classes('w-full gap-0'):
+            with ui.card():
+                with ui.grid(columns='auto auto 600px'):
+                    ui.space()
+                    ui.space()
+                    self.selectLodges = selectMulti(self.db.lodges(), 'lodge', )
+
+                    self.radioReservesAndOr = andOrRadio()
+                    ui.space()
+                    self.selectReserves = selectMulti(reservesOptions(), 'reserve', )
+
+                    self.radioMedalsAndOr = andOrRadio()
+                    ui.space()
+                    self.selectMedals = selectMulti(medalOptions(), 'medal')
+
+                    self.radioAnimalsAndOr = andOrRadio()
+                    ui.space()
+                    self.selectAnimals = selectMulti(animalsOptions(), 'animal')
+
+                with ui.row():
+                    ui.button(text='FILTER', on_click=self._updateGrid)
+                    ui.button(text='CLEAR', on_click=self._clear)
+                    self.checkboxAllAnimals = ui.checkbox(text='All animals')
+
+            with ui.card():
+                with ui.card():
+                    with ui.card():
+                        ui.label('LODGE FILE ' + ('FOUND' if self.trophyFileExists else 'NOT FOUND'))
+                    self.upload_component = ui.upload(label='UPLOAD LODGE FILE',
+                                                 on_upload=self._uploadLodge,
+                                                 multiple=False,
+                                                 auto_upload=True).props('accept="*"').tooltip(
+                        'Upload trophy_lodges_adf file')
+                    with ui.row():
+                        ui.button(text='RELOAD', on_click=self._reload)
+                        ui.button(text='RESET', on_click=self._reset)
+
+                with ui.card():
+                    with ui.row():
+                        self.selectPresets = ui.select(options=self._presets(), label='presets',
+                                                  on_change=self._applyPreset).classes('w-48')
+                        ui.button(text='+', on_click=self.addPresetDialog.open)
+                        ui.button(text='-', on_click=self._removePreset)
+
+            self.dataGrid = ui.aggrid({
+                'defaultColDef': {'sortable': True},
+                'columnDefs': [
+                    {'headerName': 'Lodge', 'field': 'lodge', 'width': '100'},
+                    {'headerName': 'Reserve', 'field': 'reserve'},
+                    {'headerName': 'Medal', 'field': 'medal', 'width': '100'},
+                    {'headerName': 'Animal', 'field': 'animal'},
+                    {'headerName': 'Rating', 'field': 'rating', 'width': '100'},
+                    {'headerName': 'Weight', 'field': 'weight', 'width': '100'},
+                    {'headerName': 'Difficulty', 'field': 'difficulty', 'width': '100'},
+                    {'headerName': 'Fur type', 'field': 'furType', 'width': '100'},
+                    {'headerName': 'Datetime', 'field': 'datetime', 'sort': 'desc'},
+                ],
+                'pagination': True,
+                'paginationPageSize': 50,
+                'rowData': self._getRowData()
+            }, html_columns=[0]).style('height: 600px').classes('col-span-full border p-1')
+
+            footer()
+
+    def _getDb(self):
+        return self.db
+
+    def _getRowData(self) -> list[dict]:
+        return rowData(self._getDb().trophyAnimals(self.queries.queryDict))
+
+    async def _uploadLodge(self, e):
         if e.content:
             temp_dir = Path(tempfile.mkdtemp())
             temp_file_path = temp_dir / 'trophy_lodges_adf'
@@ -32,150 +125,77 @@ def homePage(paths=Paths(get_save_path())):
                 e.content.seek(0)  # Reset file pointer to beginning
                 f.write(e.content.read())
 
-            paths.updateLoadPath(temp_dir)
+            self.paths.updateLoadPath(temp_dir)
             ui.notify('Trophy file uploaded successfully!', type='positive')
 
-    def reset():
-        paths.resetToDefaultPath()
-        upload_component.reset()
+    def _reset(self):
+        self.paths.resetToDefaultPath()
+        self.upload_component.reset()
 
-    def presets() -> dict:
-        return db.presets()
+    def _presets(self) -> dict:
+        return self.db.presets()
 
-    @binding.bindable_dataclass
-    class Data:
-        text: str
+    def _removePreset(self):
+        self.db.presetRemove(self.selectPresets.value)
+        self._updatePresets()
 
-    presetName = Data(text="")
+    def _addPreset(self):
+        self._updateQueries()
+        self.db.presetAdd(self.presetName.text, self.queries.queryDict)
+        self._updatePresets()
+        self.addPresetDialog.close()
 
-    addPresetDialog = ui.dialog()
-    with addPresetDialog, ui.card():
-        ui.input(label="preset name").bind_value(presetName, "text")
-        ui.button(text="save", on_click=lambda: addPreset())
+    def _updatePresets(self):
+        new_presets = self.db.presets()
+        self.selectPresets.set_options(new_presets)
+        self.selectPresets.set_value(list(new_presets.keys())[-1])
 
-    with ui.grid(columns='3fr 1fr').classes('w-full gap-0'):
-        with ui.card():  # filter
-            with ui.grid(columns='auto auto 600px'):
-                ui.space()
-                ui.space()
-                selectLodges = selectMulti(db.lodges(), 'lodge', )
+    def _applyPreset(self, e):
+        self._clearForm()
+        preset = self.db.preset(e.value)
+        self._updateFilterFromPreset(preset)
 
-                radioReservesAndOr = andOrRadio()
-                ui.space()
-                selectReserves = selectMulti(reservesOptions(), 'reserve', )
+    def _updateFilterFromPreset(self, preset: dict):
+        self.selectLodges.set_value(preset['lodges'])
+        self.radioReservesAndOr.set_value(preset['medals'])
+        self.selectReserves.set_value(preset['reserves'])
+        self.radioMedalsAndOr.set_value(preset['medalsAndOr'])
+        self.selectMedals.set_value(preset['medals'])
+        self.radioAnimalsAndOr.set_value(preset['animalsAndOr'])
+        self.selectAnimals.set_value(preset['animals'])
+        self.checkboxAllAnimals.set_value(preset['allAnimals'])
 
-                radioMedalsAndOr = andOrRadio()
-                ui.space()
-                selectMedals = selectMulti(medalOptions(), 'medal')
+    def _clearForm(self):
+        self.selectLodges.set_value('')
+        self.selectReserves.set_value('')
+        self.selectMedals.set_value('')
+        self.selectAnimals.set_value('')
+        self.checkboxAllAnimals.set_value(False)
 
-                radioAnimalsAndOr = andOrRadio()
-                ui.space()
-                selectAnimals = selectMulti(animalsOptions(), 'animal')
+    def _clear(self):
+        self._clearForm()
+        self.selectPresets.set_value('')
 
-            with ui.row():
-                ui.button(text='FILTER', on_click=lambda: updateGrid())
-                ui.button(text='CLEAR', on_click=lambda: clear())
-                checkboxAllAnimals = ui.checkbox(text='All animals')
+    def _updateQueries(self):
+        self.queries.updateQuery('lodges', self.selectLodges.value)
+        self.queries.updateQuery('reservesAndOr', self.radioReservesAndOr.value)
+        self.queries.updateQuery('reserves', self.selectReserves.value)
+        self.queries.updateQuery('medalsAndOr', self.radioMedalsAndOr.value)
+        self.queries.updateQuery('medals', self.selectMedals.value)
+        self.queries.updateQuery('animalsAndOr', self.radioAnimalsAndOr.value)
+        self.queries.updateQuery('animals', self.selectAnimals.value)
+        self.queries.updateQuery('allAnimals', self.checkboxAllAnimals.value)
 
-        with ui.card():
-            with ui.card():
-                with ui.card():
-                    ui.label('LODGE FILE ' + ('FOUND' if trophyFileExists else 'NOT FOUND'))
-                upload_component = ui.upload(label='UPLOAD LODGE FILE',
-                                             on_upload=lambda e: uploadLodge(e),
-                                             multiple=False,
-                                             auto_upload=True).props('accept="*"').tooltip(
-                    'Upload trophy_lodges_adf file')
-                with ui.row():
-                    ui.button(text='RELOAD', on_click=lambda: reload())
-                    ui.button(text='RESET', on_click=lambda: reset())
+    def _updateGrid(self):
+        self._updateQueries()
+        self.dataGrid.options['rowData'] = self._getRowData()
+        self.dataGrid.update()
 
-            with ui.card():
-                with ui.row():
-                    selectPresets = ui.select(options=presets(), label='presets',
-                                              on_change=lambda e: applyPreset(e)).classes('w-48')
-                    ui.button(text='+', on_click=addPresetDialog.open)
-                    ui.button(text='-', on_click=lambda: removePreset())
-
-        dataGrid = ui.aggrid({
-            'defaultColDef': {'sortable': True},
-            'columnDefs': [
-                {'headerName': 'Lodge', 'field': 'lodge', 'width': '100'},
-                {'headerName': 'Reserve', 'field': 'reserve'},
-                {'headerName': 'Medal', 'field': 'medal', 'width': '100'},
-                {'headerName': 'Animal', 'field': 'animal'},
-                {'headerName': 'Rating', 'field': 'rating', 'width': '100'},
-                {'headerName': 'Weight', 'field': 'weight', 'width': '100'},
-                {'headerName': 'Difficulty', 'field': 'difficulty', 'width': '100'},
-                {'headerName': 'Fur type', 'field': 'furType', 'width': '100'},
-                {'headerName': 'Datetime', 'field': 'datetime', 'sort': 'desc'},
-            ],
-            'pagination': True,
-            'paginationPageSize': 50,
-            'rowData': (lambda: getRowData())()
-        }, html_columns=[0]).style('height: 600px').classes('col-span-full border p-1')
-
-    def removePreset():
-        db.presetRemove(selectPresets.value)
-        _updatePresets()
-
-    def addPreset():
-        updateQueries()
-        db.presetAdd(presetName.text, queries.queryDict)
-        _updatePresets()
-        addPresetDialog.close()
-
-    def _updatePresets():
-        new_presets = db.presets()
-        selectPresets.set_options(new_presets)
-        selectPresets.set_value(list(new_presets.keys())[-1])
+    def _reload(self):
+        self.db = Db(self.paths.getLoadPath())
+        self.selectLodges.set_options(self.db.lodges())
+        self._updateGrid()
 
 
-    def applyPreset(e):
-        clearForm()
-        preset = db.preset(e.value)
-        updateFilterFromPreset(preset)
-
-    def updateFilterFromPreset(preset: dict):
-        selectLodges.set_value(preset['lodges'])
-        radioReservesAndOr.set_value(preset['medals'])
-        selectReserves.set_value(preset['reserves'])
-        radioMedalsAndOr.set_value(preset['medalsAndOr'])
-        selectMedals.set_value(preset['medals'])
-        radioAnimalsAndOr.set_value(preset['animalsAndOr'])
-        selectAnimals.set_value(preset['animals'])
-        checkboxAllAnimals.set_value(preset['allAnimals'])
-
-    def clearForm():
-        selectLodges.set_value('')
-        selectReserves.set_value('')
-        selectMedals.set_value('')
-        selectAnimals.set_value('')
-        checkboxAllAnimals.set_value(False)
-
-    def clear():
-        clearForm()
-        selectPresets.set_value('')
-
-    def updateQueries():
-        queries.updateQuery('lodges', selectLodges.value)
-        queries.updateQuery('reservesAndOr', radioReservesAndOr.value)
-        queries.updateQuery('reserves', selectReserves.value)
-        queries.updateQuery('medalsAndOr', radioMedalsAndOr.value)
-        queries.updateQuery('medals', selectMedals.value)
-        queries.updateQuery('animalsAndOr', radioAnimalsAndOr.value)
-        queries.updateQuery('animals', selectAnimals.value)
-        queries.updateQuery('allAnimals', checkboxAllAnimals.value)
-
-    def updateGrid():
-        updateQueries()
-        dataGrid.options['rowData'] = getRowData()
-        dataGrid.update()
-
-    def reload():
-        nonlocal db
-        db = Db(paths.getLoadPath())
-        selectLodges.set_options(db.lodges())
-        updateGrid()
-
-    footer()
+def homePage(paths=Paths(get_save_path())):
+    HomePage(paths)
