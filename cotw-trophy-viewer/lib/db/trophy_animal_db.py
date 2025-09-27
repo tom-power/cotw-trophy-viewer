ca015b8cd4d6e18a4dc40cddea78a5d403c6c452
Tom Power <tpower21@gmail.com>
2025-09-27 10:05:39 +0100

    drop TrophyAnimals table before first load

--- cotw-trophy-viewer/lib/db/trophy_animal_db.py
+++ cotw-trophy-viewer/lib/db/trophy_animal_db.py
@@ -19,8 +19,9 @@ class TrophyAnimalDb:
         conn = sqlite3.connect(self.db_path)
         cursor = conn.cursor()
 
-        cursor.execute('''
-               CREATE TABLE IF NOT EXISTS TrophyAnimals (
+        cursor.execute('DROP TABLE TrophyAnimals')
+        cursor.execute('''               
+               CREATE TABLE TrophyAnimals (
                    id TEXT PRIMARY KEY,
                    type INTEGER,
                    weight REAL,
