206a07e91710776c87feba6988f482f4d09fe4c9
Tom Power <tpower21@gmail.com>
2025-10-14 08:24:24 +0100

    drop docker

--- sh/runDocker.sh
+++
@@ -1,3 +0,0 @@
-cd ./docker &&
-docker compose build cotw_trophy_viewer &&
-docker compose up --detach --force-recreate --remove-orphans cotw_trophy_viewer
--- sh/runDockerDeploy.sh
+++
@@ -1,3 +0,0 @@
-cd ./docker &&
-docker compose -f docker-compose-deploy.yml build cotw_trophy_viewer &&
-docker compose -f docker-compose-deploy.yml up --detach --force-recreate --remove-orphans cotw_trophy_viewer
