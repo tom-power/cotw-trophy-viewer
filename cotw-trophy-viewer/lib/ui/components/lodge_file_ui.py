35d4dd710165d2370e9df22b523073caafeb082a
Tom Power <tpower21@gmail.com>
2025-10-18 08:20:14 +0100

    add auto reload checkbox

--- cotw-trophy-viewer/lib/ui/components/lodge_file_ui.py
+++ cotw-trophy-viewer/lib/ui/components/lodge_file_ui.py
@@ -2,6 +2,8 @@ import tempfile
 from pathlib import Path
 
 from nicegui import ui
+from watchdog.events import FileSystemEventHandler, FileSystemEvent
+from watchdog.observers import Observer
 
 from lib.load.loader import Loader
 
@@ -10,18 +12,24 @@ class LodgeFileUi:
     def __init__(self, loader: Loader, reloadFromFile):
         self.loader = loader
         self.reloadFromFile = reloadFromFile
+        self.observer = Observer()
         self._build_ui()
 
     def _build_ui(self):
         with ui.card():
-            with ui.card():
-                self.status_label = ui.label()
-                self.status_label.bind_text_from(self, 'status')
-            self.upload_component = ui.upload(label='UPLOAD LODGE FILE',
-                                              on_upload=self._loadLodgeFile,
-                                              multiple=False,
-                                              auto_upload=True
-                                              ).props('accept="*"').tooltip('Upload trophy_lodges_adf file')
+            with ui.row():
+                with ui.card():
+                    self.status_label = ui.label()
+                    self.status_label.bind_text_from(self, 'status')
+                if self.loader.loadFileExists():
+                    ui.checkbox(text='auto reload', on_change=self._auto_reload, value=True)
+            self.upload_component = (ui
+                                     .upload(label='UPLOAD LODGE FILE',
+                                             on_upload=self._loadLodgeFile,
+                                             multiple=False,
+                                             auto_upload=True)
+                                     .props('accept="*"')
+                                     .tooltip('Upload trophy_lodges_adf file'))
             with ui.row():
                 ui.button(text='RESET', on_click=self._reset)
                 ui.button(text='RELOAD', on_click=self._reload)
@@ -36,7 +44,7 @@ class LodgeFileUi:
             temp_file_path = temp_dir / 'trophy_lodges_adf'
 
             with open(temp_file_path, 'wb') as f:
-                e.content.seek(0)  # Reset file pointer to beginning
+                e.content.seek(0)
                 f.write(e.content.read())
 
             self.loader.updateLoadPath(temp_file_path)
@@ -50,4 +58,22 @@ class LodgeFileUi:
         ui.notify('Reset successfully!', type='positive')
 
     def _reload(self):
+        self.reloadFromFile()
+
+    def _auto_reload(self, e):
+        if e.value:
+            self.observer.schedule(
+                event_handler=ReloadEventHandler(self.reloadFromFile),
+                path=str(self.loader.paths.getLoadPath().resolve()),
+                recursive=True)
+            self.observer.start()
+        else:
+            self.observer.stop()
+            self.observer.join()
+
+class ReloadEventHandler(FileSystemEventHandler):
+    def __init__(self, reloadFromFile):
+        self.reloadFromFile = reloadFromFile
+
+    def on_any_event(self, event: FileSystemEvent) -> None:
         self.reloadFromFile()
